0f52dcec25fea1d313eb2a6a54c503545b73940a
Vladyslav Dovhal <vladyslav_dovhal@epam.com>
2022-02-27 12:39:19 +0200

    Initial commit

---
+++ app/src/main/java/com/airalarmua/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.airalarmua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.airalarmua.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
